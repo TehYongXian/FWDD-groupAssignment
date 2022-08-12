8ec4b4cf0784aeee24e096ca7805d6250ca76234
TehYongXian <yongxianteh@users.noreply.github.com>
2022-08-13 04:54:02 +0800

    update some interface such as feedback.html

--- php/login.php
+++ php/login.php
@@ -1,46 +1,32 @@
 <?php
-session_start();
-include "db_conn.php";
 
-if(isset($_POST['username']) && isset($_POST['password'])) {
-    function validate($data) {
-        $data = trim($data) 
-        $data = stripslahes($data);
-        $data = htmlspecialchars($data);
-        return data;
+$username = isset($_POST['username'])  ? $_POST['username']: '';
+$username = isset($_POST['password'])  ? $_POST['password']: '';
+
+$ok = true;
+$messages = array();
+
+if (!isset($username) || empty($password)) {
+    $ok = false;
+    $messages[] = 'Password cannot be empty!';
+}
+
+if ($ok) {
+    if ($username === 'dcode' && $password === 'dcode') {
+        $ok = true;
+        $messages[] = 'Successful Login!';
+    } else {
+        $ok = false;
+        $messages[] = 'Incorrect username/password';
     }
 }
 
-$username = validate($_POST['username']);
-$pass = validate($_POST['password']);
 
-if(empty($username)) {
-    header ("Location: index.php?erro=User Name is required");
-    exit();
-} else if (empty($pass)) {
-    header ("Location: index.php?erro=Password is required");
-    exit();
-}
+echo json_encode(
+    array(
+        'ok' => $ok,
+        'message' => $messages
+    )
+    );
 
-$sql = "SELECT * FROM users WHERE user_name='$username' AND password='$pass'";
-
-$result = mysqli_query($conn, $sql);
-
-if(mysqli_num_rows($result) === 1) {
-    $row = mysqli_fetch_assoc($result);
-    if($row['user_name'] === $username && $row['password'] === $pass) {
-        echo "Logged in";
-        $_SESSION['user_name'] = $row['user_name'];
-        $_SESSION['name'] = $row['name'];
-        $_SESSION['id'] = $row['id'];
-        header("Location: /html/home.html")
-        exit();
-    }
-    else {
-        header("Location: /html/loginRegister.html")
-        exit();
-    }
-} else {
-    header("Location: /html/loginRegister.html")
-    exit();
-}
+?>
